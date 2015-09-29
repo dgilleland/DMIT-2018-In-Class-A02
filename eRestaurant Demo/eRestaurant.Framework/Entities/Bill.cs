67a6d67bf91f5df1aa6e5c036ce8dad6a27103c8
Dan Gilleland <dgilleland@nait.ca>
2015-09-29 15:21:06 -0600

    Bill and Waiter class

---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Bill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities
+{
+    public class Bill
+    {
+        public int BillID { get; set; }
+        public DateTime BillDate { get; set; }
+        public DateTime? OrderPlaced { get; set; }
+        public int NumberInParty { get; set; }
+        public bool PaidStatus { get; set; }
+        public int WaiterID { get; set; }
+        public int? TableID { get; set; }
+        public int? ReservationID { get; set; }
+        public bool OrderReady { get; set; }
+        public string Comment { get; set; }
+
+        // Navigation Properties
+        public virtual Waiter Waiter { get; set; }
+        public virtual Table Table { get; set; }
+
+        public Bill()
+        {
+            BillDate = DateTime.Now;
+        }
+    }
+}
---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Waiter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities
+{
+    public class Waiter
+    {
+        public int WaiterID { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(25)]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(35)]
+        public string LastName { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(15, MinimumLength = 4)]
+        public string Phone { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(30, MinimumLength = 8)]
+        public string Address { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        // Navigation Properties
+        public virtual ICollection<Bill> Bills { get; set; }
+    }
+}
