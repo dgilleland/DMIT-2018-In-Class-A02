5860b79e92fc49e0abb9b867da1b4a54f312b94b
Dan Gilleland <dgilleland@nait.ca>
2015-10-19 15:33:13 -0600

    Reporting Basics - POCO class & BLL

---
+++ eRestaurant Demo/eRestaurant.Framework/BLL/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using eRestaurant.Framework.Entities.POCOs; // for CategoryMenuItem
+using eRestaurant.Framework.DAL;
+
+namespace eRestaurant.Framework.BLL
+{
+    [DataObject]
+    public class ReportController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<CategoryMenuItem> GetReportCategoryMenuItems()
+        {
+            using(var context = new RestaurantContext())
+            {
+                var results = from cat in context.Items
+                              orderby cat.MenuCategory.Description, cat.Description
+                              select new CategoryMenuItem()
+                              {
+                                  CategoryDescription = cat.MenuCategory.Description,
+                                  ItemDescription = cat.Description,
+                                  Price = cat.CurrentPrice,
+                                  Calories = cat.Calories,
+                                  Comment = cat.Comment
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/POCOs/CategoryMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities.POCOs
+{
+    // This POCO is being used for Reporting.
+    public class CategoryMenuItem
+    {
+        public string CategoryDescription { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal Price { get; set; }
+        public int? Calories { get; set; }
+        public string Comment { get; set; }
+    }
+}
