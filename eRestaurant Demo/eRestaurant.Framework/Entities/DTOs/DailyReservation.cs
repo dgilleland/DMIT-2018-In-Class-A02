3c36c8beb17306d2bc697773f3c6872c893690a1
Dan Gilleland <dgilleland@nait.ca>
2015-10-22 16:24:26 -0600

    POCOs/DTOs for Upcoming Reservations

---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/DTOs/DailyReservation.cs
@@ -0,0 +1,16 @@
+﻿using eRestaurant.Framework.Entities.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities.DTOs
+{
+    public class DailyReservation
+    {
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public IEnumerable<Booking> Reservations { get; set; }
+    }
+}
---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/POCOs/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace eRestaurant.Framework.Entities.POCOs
+{
+    public class Booking
+    {
+        public string Name { get; set; }
+        public TimeSpan Time { get; set; }
+        public int NumberInParty { get; set; }
+        public string Phone { get; set; }
+        public string Event { get; set; }
+        /*
+             select new // Booking() // Create a Booking POCO class
+					{
+						Name = booking.CustomerName,
+						Time = booking.ReservationDate.TimeOfDay,
+						NumberInParty = booking.NumberInParty,
+						Phone = booking.ContactPhone,
+						Event = booking.SpecialEvents.Description
+					}
+        */
+    }
+}
