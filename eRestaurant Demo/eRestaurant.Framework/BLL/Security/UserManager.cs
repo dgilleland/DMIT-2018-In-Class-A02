6dfbc8e9abda7af81b6c0f48b05d738f09af1485
Dan Gilleland <dgilleland@nait.ca>
2015-10-27 16:18:22 -0600

    Constants for UserManager defaults

--- eRestaurant Demo/eRestaurant.Framework/BLL/Security/UserManager.cs
+++ eRestaurant Demo/eRestaurant.Framework/BLL/Security/UserManager.cs
@@ -12,9 +12,22 @@ namespace eRestaurant.Framework.BLL.Security
 {
     public class UserManager : UserManager<ApplicationUser>
     {
+        #region Constants
+        // Ideally, we should probably put some/all of these in the Web.config file
+        // underneath the <appSettings> element.
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        private const string STR_EMAIL_FORMAT = "{0}@eRestaurant.tba";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+
+        #region Constructor(s)
         public UserManager()
             : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
         {
         }
+        #endregion
+
+
     }
 }
