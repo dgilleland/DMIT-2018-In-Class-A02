4a20d6157b4a3f8d17013451b4eac8df8364c553
Dan Gilleland <dgilleland@nait.ca>
2015-11-19 15:33:54 -0700

    DTO

---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/DTOs/SeatingSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities.DTOs
+{
+    public class SeatingSummary
+    {
+        public byte Table { get; set; } // the Tables.TableNumber
+        public int Seating { get; set; } // the seating capacity - Tables.Capacity
+        public bool Taken { get; set; } // calculated - true if occupied
+        public int? BillID { get; set; } // Bills.BillID (nullable)
+        public decimal? BillTotal { get; set; } // calculated - total bill (nullable)
+        public string Waiter { get; set; } // Waiters' name (nullable)
+        public string ReservationName { get; set; } // Reservations.ContactName (nullable)
+    }
+}
